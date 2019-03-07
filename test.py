85b51944ab8c673cdfe72cae621727707f46a2f9
Charl P. Botha <charl.botha@stonethree.com>
2019-03-07 12:33:43 +0200

    Add pytest

--- test.py
+++ test.py
@@ -1,27 +1,9 @@
-import vtk
-import time
+import vtk  # noqua: F401
+import vtkzbhps
 
-cone = vtk.vtkConeSource()
-cone.SetHeight(3.0)
-cone.SetRadius(1.0)
-cone.SetResolution(10)
 
-coneMapper = vtk.vtkPolyDataMapper()
-coneMapper.SetInputConnection(cone.GetOutputPort())
-
-coneActor = vtk.vtkActor()
-coneActor.SetMapper(coneMapper)
-
-ren1 = vtk.vtkRenderer()
-ren1.AddActor(coneActor)
-ren1.SetBackground(0.1, 0.2, 0.4)
-
-renWin = vtk.vtkRenderWindow()
-renWin.AddRenderer(ren1)
-renWin.SetSize(300, 300)
-
-for i in range(0, 360):
-    time.sleep(0.03)
-
-    renWin.Render()
-    ren1.GetActiveCamera().Azimuth(1)
+def test_instantiation():
+    cpt = vtkzbhps.vtkCPTDistanceField2D()
+    assert cpt.GetDomain() == (0.0, 0.0, 0.0, 0.0)
+    pw = vtkzbhps.vtkProsthesisWidget()
+    assert pw.GetEnabled() == 0
